e62fd3727c17d8ed9ce6442c715584ff7477e904
Edward Yang <edward_yang_125@hotmail.com>
2024-04-01 12:39:03 +1100

    add species in email

--- email-and-upload.sh
+++ email-and-upload.sh
@@ -2,6 +2,8 @@
 
 set -ue
 
+email_list=$1
+
 # save the date in yyyy-mm-dd format into the "date" variable
 printf -v date '%(%Y-%m-%d)T' -1
 
@@ -14,18 +16,23 @@ rclone copy observations/ google-drive-birds:/
 # create a public, read-only link to the folder
 drive_link=$(rclone link google-drive-birds:/$date)
 
-nbirbs=$(ls observations/$date/triggers | wc -l)
+nbirbs=$(find observations/$date/instances -type f -name 'im.jpg*.jpg' | wc -l)
+
+birblist=$(for species in observations/$date/instances/*/; do echo "<li>"$(basename "$species"): $(ls "$species" | wc -l)"</li>"; done)
 
 python3 send_birb_summary.py \
     -s "Birb watcher update" \
     -i observations/$date/sample.jpg \
     eds.birb.watcher@gmail.com \
     "Birb Watcher" \
-    email-lists.csv \
+    "$email_list" \
     "<html>
     <body>
         <p>Hi {FIRST},</p>
-	<p>I saw ${nbirbs} today!</p>
+	<p>I saw ${nbirbs} today! These were:</p>
+        <ul>
+        $birblist
+        </ul>
         <p>You can view all the birds I saw today via <a href=\"${drive_link}\">this google drive link</a>!</p>
         <p>The \"triggers\" folder shows what the AI model thought was a bird.<br>The \"originals\" folder contains the unmodified images.</p>
         <p>Send Ed a message if you see a photo that doesn't actually contain a bird!<br>Those images can be used to improve the AI model's accuracy.</p>
