1e7217fed368169c11e9ca7c3d36bb82b3aaf69c
Edward Yang <94523015+edoyango@users.noreply.github.com>
2024-04-15 17:26:28 +1000

    improve augment-data.py

--- scripts/augment-data.py
+++ scripts/augment-data.py
@@ -1,75 +1,90 @@
 import glob, cv2, numpy as np, argparse, os, albumentations as A, random, shutil
-
-TRANSFORM = A.Compose(
-    [
-        A.HorizontalFlip(p=0.2),
-        A.VerticalFlip(p=0.2),
-        A.GaussianBlur(p=0.2),
-        A.GaussNoise(p=0.2),
-        A.RandomBrightnessContrast(p=0.2),
-        # # A.Affine(p=0.2),
-        # A.Perspective(always_apply=True, p=0.2),
-        A.ColorJitter(p=0.2),
-        A.Spatter(p=0.2),
-        A.Downscale(interpolation=cv2.INTER_LINEAR, p=0.2),
-    ],
-    keypoint_params=A.KeypointParams(format="xy"),
-)
+import multiprocessing as mp, itertools
 
 
 class albumentations_transformer:
-    def __init__(self, img_path, polygons_path):
+    def __init__(self, img_path, boxes_path):
         self.map = {}
         self.classes = {}
         self.data = np.empty((0))
         self.count = 0
         self.orig_img = cv2.imread(img_path)
         self.img_path = img_path
-        self.polygons_path = polygons_path
+        self.boxes_path = boxes_path
 
-        with open(polygons_path, "r") as f:
-            for line in f:
-                line_data = np.fromstring(line, sep=" ")
-                start = len(self.data)
-                self.map[self.count] = (start, start + len(line_data) - 1)
-                self.classes[self.count] = int(line_data[0])
-                self.data = np.concatenate((self.data, line_data[1:]), axis=0)
-                self.count += 1
-
-    def _albumentations(self):
-        return np.minimum(
-            self.data.reshape((-1, 2)) * np.array((1920, 1080)), np.array((1919, 1079))
-        )
-
+        self.boxes = np.loadtxt(boxes_path)
+        print(self.img_path, self.boxes_path)
+        if len(self.boxes.shape) == 1:
+            self.boxes = self.boxes[np.newaxis, :]
+        if self.boxes.size > 0:
+            self.boxes = self.boxes[:, [1, 2, 3, 4, 0]]
+        
     def _apply_transform(self, transform):
-        res = transform(image=self.orig_img, keypoints=self._albumentations())
-        if len(res["keypoints"]) > 0:
-            return res["image"], (
-                np.array(res["keypoints"]) / np.array((1920, 1080))
-            ).reshape(-1)
-        else:
-            return res["image"], np.empty((0))
 
-    def generate_images(self, transform, n):
+        if self.boxes.size > 0:
+            res = transform(image=self.orig_img, bboxes=self.boxes)
+            return res["image"], np.array(res["bboxes"])
+        else:
+            res =  transform(image=self.orig_img)
+            return res["image"], np.empty(0)
+
+    def __call__(self, n):
         idx = 0
         img_path_split = os.path.splitext(self.img_path)
-        polygons_path_split = os.path.splitext(self.polygons_path)
-        for i in range(n):
-            img_t, data_t = self._apply_transform(transform)
-            cv2.imwrite(
-                img_path_split[0] + f"_augmented{idx}" + img_path_split[1], img_t
+        polygons_path_split = os.path.splitext(self.boxes_path)
+        if self.boxes.size > 0:
+            TRANSFORM = A.Compose(
+                [
+                    A.HorizontalFlip(p=0.2),
+                    A.VerticalFlip(p=0.2),
+                    A.GaussianBlur(p=0.2),
+                    A.GaussNoise(p=0.2),
+                    A.RandomBrightnessContrast(p=0.2),
+                    #A.Affine(p=0.2),
+                    A.Perspective(always_apply=True, p=0.2),
+                    A.ColorJitter(p=0.2),
+                    A.Spatter(p=0.2),
+                    A.Downscale(interpolation=cv2.INTER_LINEAR, p=0.2),
+                ],
+                bbox_params=A.BboxParams(format="yolo"),
             )
-            with open(
-                polygons_path_split[0] + f"_augmented{idx}" + polygons_path_split[1],
-                "w",
-            ) as f:
-                for k, v in self.map.items():
-                    line = str(self.classes[k])
-                    for j in data_t[v[0] : v[1]]:
-                        line += f" {j}"
-                    line += "\n"
-                    f.write(line)
+        else:
+            TRANSFORM = A.Compose(
+                [
+                    A.HorizontalFlip(p=0.2),
+                    A.VerticalFlip(p=0.2),
+                    A.GaussianBlur(p=0.2),
+                    A.GaussNoise(p=0.2),
+                    A.RandomBrightnessContrast(p=0.2),
+                    #A.Affine(p=0.2),
+                    A.Perspective(always_apply=True, p=0.2),
+                    A.ColorJitter(p=0.2),
+                    A.Spatter(p=0.2),
+                    A.Downscale(interpolation=cv2.INTER_LINEAR, p=0.2),
+                ],
+            )
+        outfiles = []
+        for i in range(n):
+            img_t, data_t = self._apply_transform(TRANSFORM)
+            cv2.imwrite(
+                img_path_split[0] + f"_augmented{idx}" + img_path_split[1], img=img_t
+            )
+            try:
+                outdata = data_t[:, [4, 0, 1, 2, 3]]
+                fmt = ["%d", "%f", "%f", "%f", "%f"]
+            except:
+                outdata = data_t
+                fmt = "%f"
+            fpath = polygons_path_split[0] + f"_augmented{idx}" + polygons_path_split[1]
+            np.savetxt(fpath, outdata, fmt=fmt)
             idx += 1
+            outfiles.append(fpath)
+        return outfiles
+
+def run_transformer(imgpath, lblpath, nfiles):
+
+    a = albumentations_transformer(imgpath, lblpath)
+    return a(nfiles)
 
 
 if __name__ == "__main__":
@@ -77,60 +92,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("dir")
     parser.add_argument("nfiles", type=int)
+    parser.add_argument("--nprocs", "-n", default=1, type=int)
     args = parser.parse_args()
 
-    images = sorted(glob.glob(os.path.join(args.dir, "*", "images", "*")))
-    labels = sorted(glob.glob(os.path.join(args.dir, "*", "labels", "*")))
-
-    for imgpath, lblpath in zip(images, labels):
-
-        a = albumentations_transformer(imgpath, lblpath)
-        a.generate_images(TRANSFORM, args.nfiles)
-
-    images = sorted(glob.glob(os.path.join(args.dir, "*", "images", "*")))
-    labels = sorted(glob.glob(os.path.join(args.dir, "*", "labels", "*")))
-
-    data = []
-    for i, l in zip(images, labels):
-        data.append((i, l))
-
-    ntotal = len(data)
-    ntest = max(1, int(0.1 * ntotal))
-    nval = max(1, int(0.2 * ntotal))
-    ntrain = len(images) - ntest - nval
-    if ntest < 1 or nval < 1 or ntrain < 1:
-        raise ("Not enough samples")
-
-    random.shuffle(data)
-
-    train_data = data[0:ntrain]
-    val_data = data[ntrain : ntrain + nval]
-    test_data = data[ntrain + nval : ntrain + nval + ntest]
-
-    train_dir = os.path.join(args.dir, "train")
-    val_dir = os.path.join(args.dir, "valid")
-    test_dir = os.path.join(args.dir, "test")
-
-    for img, lbl in train_data:
-        new_img_path = os.path.join(test_dir, "images")
-        new_lbl_path = os.path.join(test_dir, "labels")
-        if img != new_img_path:
-            shutil.move(img, new_img_path)
-        if lbl != new_lbl_path:
-            shutil.move(lbl, new_lbl_path)
-
-    for img, lbl in val_data:
-        new_img_path = os.path.join(val_dir, "images")
-        new_lbl_path = os.path.join(val_dir, "labels")
-        if img != new_img_path:
-            shutil.move(img, new_img_path)
-        if lbl != new_lbl_path:
-            shutil.move(lbl, new_lbl_path)
-
-    for img, lbl in test_data:
-        new_img_path = os.path.join(test_dir, "images")
-        new_lbl_path = os.path.join(test_dir, "labels")
-        if img != new_img_path:
-            shutil.move(img, new_img_path)
-        if lbl != new_lbl_path:
-            shutil.move(lbl, new_lbl_path)
+    images = sorted(glob.glob(os.path.join(args.dir, "train", "images", "*")))
+    labels = sorted(glob.glob(os.path.join(args.dir, "train", "labels", "*")))
+    
+    with mp.Pool(processes=args.nprocs) as pool:
+        files = pool.starmap(
+            run_transformer, 
+            zip(images, labels, itertools.repeat(args.nfiles))
+        )
