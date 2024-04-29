f10c6b4b3e2f3842745d2975fd07d85404f37e92
Edward Yang <yang.e@wehi.edu.au>
2024-04-29 21:37:22 +1000

    add subclassification

--- scripts/extract-birbs.py
+++ scripts/extract-birbs.py
@@ -2,6 +2,9 @@ import cv2
 import os
 import datetime
 import sys
+import torch
+import copy
+import ultralytics
 
 def open_video(vname):
 
@@ -13,8 +16,6 @@ def open_video(vname):
     return cap
 
 def get_model(model_path):
-
-    import ultralytics
 
     model = ultralytics.YOLO(model_path)
 
@@ -55,7 +56,50 @@ def create_new_fname(vidpath, frame_idx, fps, outdir=None, prefix=""):
         outpath = os.path.join(vid_dir, outfile)
     return outpath
 
-def main(vidpath, model, outdir):
+def subclassify(detection_result, cls_model):
+
+    ret = copy.deepcopy(detection_result)
+
+    # clone results so classes can be edited
+    tmp_box_data = ret.boxes.data.clone()
+
+    # extract the detected parts of the image and classify
+    res = cls_model(
+        [
+            ret.orig_img[box[1]:box[3], box[0]:box[2], :]
+            for box in tmp_box_data.round().int()
+        ],
+        imgsz=64,
+        verbose=False
+    )
+
+    # replace subclass if classifier is highly confident
+    if res[0].probs.top1conf.item() > 0.8:
+        # extract existing classes
+        nexisting_names = len(ret.names)
+        tmp_box_data[:, -1] = torch.Tensor([i.probs.top1 + nexisting_names for i in res])
+        ret.boxes.data = tmp_box_data
+        species_names = res[0].names
+        new_names = ret.names
+        for k, v in species_names.items():
+            new_names[k  + nexisting_names] = v
+        ret.names = new_names
+
+    return ret
+
+def count_detections(detection_result):
+
+    counts = {}
+    for i in detection_result.boxes.cls.cpu().int():
+        ii = int(i)
+        if counts.get(ii):
+            counts[ii] += 1
+        else:
+            counts[ii] = 1
+    
+    return {detection_result.names[k]: v for k, v in counts.items()}
+
+def main(vidpath, model_detect_path, outdir, model_cls_path = None):
 
     # setting up output directory
     os.makedirs(os.path.join(outdir, "triggers"), exist_ok=True)
@@ -63,8 +107,21 @@ def main(vidpath, model, outdir):
 
     cap = open_video(vidpath)
     fps = cap.get(cv2.CAP_PROP_FPS)
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-    model, bird_class_idx = get_model(model)
+    model, bird_class_idx = get_model(model_detect_path)
+
+    model_cls = ultralytics.YOLO(model_cls_path) if model_cls_path else None
+
+    print(f"""INPUT VIDEO: {vidpath}
+    Resolution: {cap.get(cv2.CAP_PROP_FRAME_HEIGHT)}x{int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))}
+    FPS:        {fps}
+OUTPUT DIRECTORY: {outdir}/{{originals,triggers}}
+DETECTION MODEL: {model_detect_path}
+CLASSIFICATION MODEL: {model_cls_path}
+
+Beginning processing...
+""")
 
     trigger_out_vid = None
     original_out_vid = None
@@ -74,11 +131,10 @@ def main(vidpath, model, outdir):
     WAITLIMIT=2*fps # wait two seconds before closing video
     wait_counter = WAITLIMIT
     while success:
-        if nframe % 10 == 0:
-            print(f"frame {nframe}")
-        success, frame = cap.read() 
+        success, frame = cap.read()
       
         if success:
+            print(f"frame {nframe+1}/{total_frames}", end=" ")
             # perform inference
             yolo_res = model(
                 source=frame,
@@ -87,6 +143,7 @@ def main(vidpath, model, outdir):
                 conf=0.516,
                 iou=0.5,
                 imgsz=864,
+                verbose=False
             )[0]
             # save bool indicating whether a bird was detected
             bird_detected = bird_class_idx in yolo_res.boxes.cls
@@ -94,6 +151,14 @@ def main(vidpath, model, outdir):
             wait_counter = 0 if bird_detected else wait_counter + 1
             # keep writing to video if wait limit hasn't been reached
             if wait_counter < WAITLIMIT:
+                # subclassify
+                if bird_detected:
+                    if model_cls: yolo_res = subclassify(yolo_res, model_cls)
+                    for k, v in count_detections(yolo_res).items():
+                        print(f"{k}: {v}", end=" ")
+                    print()
+                else:
+                    print("no bird detected")
                 # get annotated frame from yolo results
                 annotated_frame = yolo_res.plot(conf=False)
                 original_frame = yolo_res.orig_img
@@ -110,6 +175,7 @@ def main(vidpath, model, outdir):
                 if cv2.waitKey(1) & 0xFF == ord('s'):
                     break
             else:
+                print("no bird detected")
                 # close the output video if it's open
                 if trigger_out_vid and trigger_out_vid.isOpened():
                     # release video resources
@@ -141,7 +207,8 @@ def main(vidpath, model, outdir):
             nframe += 1
     
     cap.release() 
-    out_vid.release() 
+    if trigger_out_vid: trigger_out_vid.release() 
+    if original_out_vid: original_out_vid.release()
         
     # Closes all the frames 
     cv2.destroyAllWindows() 
@@ -156,5 +223,6 @@ if __name__ == "__main__":
     parser.add_argument("--video", "-v")
     parser.add_argument("--model", "-m", default="yolov8n.pt")
     parser.add_argument("--output-directory", "-o", default=".")
+    parser.add_argument("--cls-model", "-c", default=None)
     args = parser.parse_args()
-    main(args.video, args.model, args.output_directory)
+    main(args.video, args.model, args.output_directory, args.cls_model)
