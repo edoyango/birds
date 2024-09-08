96988fe7106b6a364f6eeb975d7acdfc9fecf82f
Edward Yang <yang.e@wehi.edu.au>
2024-09-09 08:27:57 +1000

    adapt for new mount reqs

--- scripts/run-train.sh
+++ scripts/run-train.sh
@@ -6,18 +6,22 @@
 #SBATCH -p gpuq
 #SBATCH --gres gpu:A30:4
 #SBATCH --qos bonus
+#SBATCH --requeue
 
 set -ue
 version=$1
 date=$2
 imgsz=$3
 
-module load apptainer/1.2.3
+module load apptainer/1.3.3
 
-apptainer exec -B /vast -B /stornext --nv docker://ultralytics/ultralytics:8.2.18 \
+CONTAINER=docker://ultralytics/ultralytics:8.2.79
+MOUNTS="/vast,/stornext,/vast/scratch/users/yang.e/birds/runs:/ultralytics/runs,/vast/scratch/users/yang.e/datasets:/ultralytics/datasets"
+
+apptainer exec -B "$MOUNTS" --nv $CONTAINER \
 	yolo train \
 		data=../datasets/birds-$version/data.yaml \
-		model=models/yolov9-birbs.pt \
+		model=yolov10b.pt \
 		pretrained=True \
 		epochs=300 \
 		imgsz=$imgsz \
@@ -34,10 +38,12 @@ apptainer exec -B /vast -B /stornext --nv docker://ultralytics/ultralytics:8.2.1
 
 cp ../datasets/birds-$version/data.yaml ../datasets/birds-$version/data-val.yaml
 sed -i 's@val: valid/images@val: test/images@g' ../datasets/birds-$version/data-val.yaml
-apptainer exec -B /vast -B /stornext --nv docker://ultralytics/ultralytics:8.2.18 \
+apptainer exec -B "$MOUNTS" --nv $CONTAINER \
 	yolo detect val \
 		data=../datasets/birds-$version/data-val.yaml \
 		model=runs/detect/birds-$version-$date-$imgsz-train/weights/best.pt \
 		name=birds-$version-$date-$imgsz-test \
 		exist_ok=True \
 		imgsz=$imgsz
+
+sbatch --wrap "apptainer exec -B "$MOUNTS" --nv $CONTAINER yolo export model=runs/detect/birds-$version-$date-$imgsz-train/weights/best.pt format=engine batch=20 half=True" -c 4 --mem 20G --gres gpu:A30:1 -p gpuq
