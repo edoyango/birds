9c17d3ba33aff47082b4552c5fb967e49fe14871
Edward Yang <yang.e@wehi.edu.au>
2024-09-09 08:28:34 +1000

    make export script more usable

--- scripts/yolo-export.sh
+++ scripts/yolo-export.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-#SBATCH --time 60 -c 16 --mem 100G --gres gpu:A30:1 -p gpuq 
+#SBATCH --time 60 -c 16 --mem 100G --gres gpu:A30:1 -p gpuq --job-name yolo-export --output %x.out
 module purge
-module load apptainer/1.2.3
-apptainer exec -B /vast,/stornext --nv docker://ultralytics/ultralytics:8.2.42 yolo export model=../models/yolov10-birbs.pt format=engine batch=20 half=True
+module load apptainer/1.3.3
+export APPTAINER_TMPDIR=/dev/shm
+apptainer exec -B /vast,/stornext --nv docker://ultralytics/ultralytics:8.2.79 yolo export model=../models/yolov10-birbs.pt format=engine batch=20 half=True
+rm -rf /dev/shm/*
