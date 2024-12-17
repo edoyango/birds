9d4d667393fba4c0a272d5f484290fa8cb151dcd
Edward Yang <94523015+edoyango@users.noreply.github.com>
2024-12-17 18:58:54 +1100

    Bring letterbox helper into script

--- scripts/extract-birbs.py
+++ scripts/extract-birbs.py
@@ -2,6 +2,7 @@ import os
 import cv2
 import sys
 import argparse
+from copy import copy
 
 # add path
 realpath = os.path.abspath(__file__)
@@ -9,7 +10,6 @@ _sep = os.path.sep
 realpath = realpath.split(_sep)
 sys.path.append(os.path.join(realpath[0]+_sep, *realpath[1:realpath.index('rknn_model_zoo')+1]))
 
-from py_utils.coco_utils import COCO_test_helper
 import numpy as np
 
 
@@ -33,6 +33,154 @@ CLASSES = ("person", "bicycle", "car","motorbike ","aeroplane ","bus ","train","
 coco_id_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 27, 28, 31, 32, 33, 34,
          35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63,
          64, 65, 67, 70, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 84, 85, 86, 87, 88, 89, 90]
+
+class Letter_Box_Info():
+    def __init__(self, shape, new_shape, w_ratio, h_ratio, dw, dh, pad_color) -> None:
+        self.origin_shape = shape
+        self.new_shape = new_shape
+        self.w_ratio = w_ratio
+        self.h_ratio = h_ratio
+        self.dw = dw 
+        self.dh = dh
+        self.pad_color = pad_color
+
+class COCO_test_helper():
+    def __init__(self, enable_letter_box = False) -> None:
+        self.record_list = []
+        self.enable_ltter_box = enable_letter_box
+        if self.enable_ltter_box is True:
+            self.letter_box_info_list = []
+        else:
+            self.letter_box_info_list = None
+
+    def letter_box(self, im, new_shape, pad_color=(0,0,0), info_need=False):
+        # Resize and pad image while meeting stride-multiple constraints
+        shape = im.shape[:2]  # current shape [height, width]
+        if isinstance(new_shape, int):
+            new_shape = (new_shape, new_shape)
+
+        # Scale ratio
+        r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+
+        # Compute padding
+        ratio = r  # width, height ratios
+        new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
+        dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
+
+        dw /= 2  # divide padding into 2 sides
+        dh /= 2
+
+        if shape[::-1] != new_unpad:  # resize
+            im = cv2.resize(im, new_unpad, interpolation=cv2.INTER_LINEAR)
+        top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
+        left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+        im = cv2.copyMakeBorder(im, top, bottom, left, right, cv2.BORDER_CONSTANT, value=pad_color)  # add border
+        
+        if self.enable_ltter_box is True:
+            self.letter_box_info_list.append(Letter_Box_Info(shape, new_shape, ratio, ratio, dw, dh, pad_color))
+        if info_need is True:
+            return im, ratio, (dw, dh)
+        else:
+            return im
+
+    def direct_resize(self, im, new_shape, info_need=False):
+        shape = im.shape[:2]
+        h_ratio = new_shape[0]/ shape[0]
+        w_ratio = new_shape[1]/ shape[1]
+        if self.enable_ltter_box is True:
+            self.letter_box_info_list.append(Letter_Box_Info(shape, new_shape, w_ratio, h_ratio, 0, 0, (0,0,0)))
+        im = cv2.resize(im, (new_shape[1], new_shape[0]))
+        return im
+
+    def get_real_box(self, box, in_format='xyxy'):
+        bbox = copy(box)
+        if self.enable_ltter_box == True:
+        # unletter_box result
+            if in_format=='xyxy':
+                bbox[:,0] -= self.letter_box_info_list[-1].dw
+                bbox[:,0] /= self.letter_box_info_list[-1].w_ratio
+                bbox[:,0] = np.clip(bbox[:,0], 0, self.letter_box_info_list[-1].origin_shape[1])
+
+                bbox[:,1] -= self.letter_box_info_list[-1].dh
+                bbox[:,1] /= self.letter_box_info_list[-1].h_ratio
+                bbox[:,1] = np.clip(bbox[:,1], 0, self.letter_box_info_list[-1].origin_shape[0])
+
+                bbox[:,2] -= self.letter_box_info_list[-1].dw
+                bbox[:,2] /= self.letter_box_info_list[-1].w_ratio
+                bbox[:,2] = np.clip(bbox[:,2], 0, self.letter_box_info_list[-1].origin_shape[1])
+
+                bbox[:,3] -= self.letter_box_info_list[-1].dh
+                bbox[:,3] /= self.letter_box_info_list[-1].h_ratio
+                bbox[:,3] = np.clip(bbox[:,3], 0, self.letter_box_info_list[-1].origin_shape[0])
+        return bbox
+
+    def get_real_seg(self, seg):
+        #! fix side effect
+        dh = int(self.letter_box_info_list[-1].dh)
+        dw = int(self.letter_box_info_list[-1].dw)
+        origin_shape = self.letter_box_info_list[-1].origin_shape
+        new_shape = self.letter_box_info_list[-1].new_shape
+        if (dh == 0) and (dw == 0) and origin_shape == new_shape:
+            return seg
+        elif dh == 0 and dw != 0:
+            seg = seg[:, :, dw:-dw] # a[0:-0] = []
+        elif dw == 0 and dh != 0 : 
+            seg = seg[:, dh:-dh, :]
+        seg = np.where(seg, 1, 0).astype(np.uint8).transpose(1,2,0)
+        seg = cv2.resize(seg, (origin_shape[1], origin_shape[0]), interpolation=cv2.INTER_LINEAR)
+        if len(seg.shape) < 3:
+            return seg[None,:,:]
+        else:
+            return seg.transpose(2,0,1)
+
+    def add_single_record(self, image_id, category_id, bbox, score, in_format='xyxy', pred_masks = None):
+        if self.enable_ltter_box == True:
+        # unletter_box result
+            if in_format=='xyxy':
+                bbox[0] -= self.letter_box_info_list[-1].dw
+                bbox[0] /= self.letter_box_info_list[-1].w_ratio
+
+                bbox[1] -= self.letter_box_info_list[-1].dh
+                bbox[1] /= self.letter_box_info_list[-1].h_ratio
+
+                bbox[2] -= self.letter_box_info_list[-1].dw
+                bbox[2] /= self.letter_box_info_list[-1].w_ratio
+
+                bbox[3] -= self.letter_box_info_list[-1].dh
+                bbox[3] /= self.letter_box_info_list[-1].h_ratio
+                # bbox = [value/self.letter_box_info_list[-1].ratio for value in bbox]
+
+        if in_format=='xyxy':
+        # change xyxy to xywh
+            bbox[2] = bbox[2] - bbox[0]
+            bbox[3] = bbox[3] - bbox[1]
+        else:
+            assert False, "now only support xyxy format, please add code to support others format"
+        
+        def single_encode(x):
+            from pycocotools.mask import encode
+            rle = encode(np.asarray(x[:, :, None], order="F", dtype="uint8"))[0]
+            rle["counts"] = rle["counts"].decode("utf-8")
+            return rle
+
+        if pred_masks is None:
+            self.record_list.append({"image_id": image_id,
+                                    "category_id": category_id,
+                                    "bbox":[round(x, 3) for x in bbox],
+                                    'score': round(score, 5),
+                                    })
+        else:
+            rles = single_encode(pred_masks)
+            self.record_list.append({"image_id": image_id,
+                                    "category_id": category_id,
+                                    "bbox":[round(x, 3) for x in bbox],
+                                    'score': round(score, 5),
+                                    'segmentation': rles,
+                                    })
+    
+    def export_to_json(self, path):
+        with open(path, 'w') as f:
+            json.dump(self.record_list, f)
 
 
 def filter_boxes(boxes, box_confidences, box_class_probs):
@@ -177,7 +325,6 @@ def setup_model(args):
         platform = 'rknn'
         from py_utils.rknn_executor import RKNN_model_container 
         model = RKNN_model_container(args.model_path, args.target, args.device_id)
-        model = ONNX_model_container(args.model_path)
     else:
         assert False, "{} is not rknn/pytorch/onnx model".format(model_path)
     print('Model-{} is {} model, starting val'.format(model_path, platform))
@@ -243,7 +390,7 @@ if __name__ == '__main__':
         outputs = model.run([img])
         boxes, classes, scores = post_process(outputs, anchors)
 
-        if args.img_show or args.img_save:
+        if args.img_save:
             print('\n\nIMG: {}'.format(img_name))
             img_p = img_src.copy()
             if boxes is not None:
