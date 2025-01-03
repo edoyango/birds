fd93efd39bad7936b880b239a736259429431521
Edward Yang <edward_yang_125@hotmail.com>
2025-01-03 21:25:19 +1100

    fix gif otpimization

--- utils/post-process.sh
+++ utils/post-process.sh
@@ -4,11 +4,12 @@ set -eu
 
 today=$(date +%Y-%m-%d)
 output_dir=/bird-detections/$today
+vid_duration=20
 
 . ~/rknn-venv/bin/activate
 
 # 1. find best videos
-bestvids=$(~/birds/scripts/find-best-vids.py --csv "$output_dir/meta.csv" --num-videos 4 --duration 10)
+bestvids=$(~/birds/scripts/find-best-vids.py --csv "$output_dir/meta.csv" --num-videos 5 --duration $vid_duration)
 
 # 2. create gifs from best videos
 gifs2stack=""
@@ -18,10 +19,10 @@ do
 	fname="$(basename ${f2g})"
 	fname="${fname%.*}"
 	duration=$(ffmpeg -i "$f2g" 2>&1 | grep "Duration" | cut -d ' ' -f 4 | sed s/,// | sed 's@\\..*@@g' | awk '{ split($1, A, ":"); print 3600*A[1] + 60*A[2] + B[1] }')
-	let "starttime = $duration/2 - 11"
+	let "starttime = $duration/2 - $vid_duration"
 	[ "$starttime" -lt 0 ] && starttime=0
-	ffmpeg -y -ss "$starttime" -t 10 -i "$f2g" \
-		-vf "scale=480:-1:flags=lanczos,split[s0][s1];[s0]palettegen=max_colors=128[p];[s1][p]paletteuse" \
+	ffmpeg -y -ss "$starttime" -t $vid_duration -i "$f2g" \
+		-vf "scale=480:-1:flags=lanczos" \
 		/tmp/"$fname.gif"
 	gifs2stack="/tmp/$fname.gif $gifs2stack"
 done
@@ -30,7 +31,7 @@ done
 ~/birds/scripts/stack-gifs.py /tmp/stacked.gif $gifs2stack
 
 # 4. optimize gif
-gifsicle -O3 --lossy=35 -i /tmp/stacked.gif -o "$output_dir/sample.gif"
+gifsicle -O3 --lossy=35 -i /tmp/stacked.gif --colors 128 -o "$output_dir/sample.gif"
 
 # 5. send email
 ## create google drive link to triggers folder
@@ -60,3 +61,4 @@ export GMAIL_APP_PWD=bpcshpyjugjpmbvy
 
 # 6. upload to google drive
 #rclone -P move ~/bird-detections gdrive:birds/$today
+
