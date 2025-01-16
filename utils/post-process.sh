ba34ec8d0653b13a8b97d52bbb1cebb770bb9814
Edward Yang <edward_yang_125@hotmail.com>
2025-01-17 07:44:50 +1100

    move post-process docker cmd into script

--- utils/post-process.sh
+++ utils/post-process.sh
@@ -5,4 +5,10 @@ set -eu
 today=$(date +%Y-%m-%d)
 output_dir=/output/$today
 
-python /app/post-process.py $output_dir $output_dir/meta.csv /app/email-lists.csv
+docker run \
+    --net birds_bird_net \
+    -v /bird-detections:/output \
+    -v ./email-lists.csv:/app/email-lists.csv:ro \
+    --entrypoint python \
+    ghcr.io/edoyango/birds \
+        /app/post-process.py $output_dir $output_dir/meta.csv /app/email-lists.csv
