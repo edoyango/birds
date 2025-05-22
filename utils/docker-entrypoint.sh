33398ce23020f180832c29b5205b7b30537ba1c7
Edward Yang <edward_yang_125@hotmail.com>
2025-05-22 20:05:37 +1000

    update docker

--- utils/docker-entrypoint.sh
+++ utils/docker-entrypoint.sh
@@ -5,6 +5,7 @@ max_frames=${2:-6000}
 prefix="${3:-}"
 prom_port="${4:-9093}"
 npu_cores="${5:-'-1'}"
+batch_size="${6:-1}"
 
 extract-birbs \
     --model_path /app/models/yolov5-birbs.rknn \
@@ -14,4 +15,5 @@ extract-birbs \
     --max-frames $max_frames \
     --video-name-prefix "$prefix" \
     --metrics-port $prom_port \
-    --npu-cores $npu_cores
+    --npu-cores $npu_cores \
+    --batch-size $batch_size
